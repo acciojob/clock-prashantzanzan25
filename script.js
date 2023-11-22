e6d2ae00670e1165171d2150b869f7f5cd5793cf
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-22 17:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+function updateTime() {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+	var date = currentTime.getDate();
+	var month = currentTime.getMonth() + 1;
+	var year = currentTime.getFullYear();
+	
+	hours = (hours<10) ? "0" + hours : hours;
+	minutes = (minutes<10) ? "0" + minutes : minutes;
+	seconds = (seconds<10) ? "0" + seconds : seconds;
+	date = (date<10) ? "0" + date : date;
+	month = (month<10) ? "0" + month : month;
+
+	var currentDateTime = date+"/"+month+"/"+year+" "+hours+":"+minutes+":"+seconds;
+
+	document.getElementById("timer").innerHTML = currentDateTime;
+}
+setInterval(updateTime, 1000);
+
+
