c4747c75c371ad90a5c4b18b062ea68f9facccd2
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-22 18:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 //your JS code here. If required.
-
-function updateTime() {
-	var currentTime = new Date();
-	var hours = currentTime.getHours();
-	var minutes = currentTime.getMinutes();
-	var seconds = currentTime.getSeconds();
-	var date = currentTime.getDate();
-	var month = currentTime.getMonth() + 1;
-	var year = currentTime.getFullYear();
+function updateTimer() {
+	const timerElement = document.getElementById('timer');
+	const now = new Date();
 	
-	hours = (hours<10) ? "0" + hours : hours;
-	minutes = (minutes<10) ? "0" + minutes : minutes;
-	seconds = (seconds<10) ? "0" + seconds : seconds;
-	date = (date<10) ? "0" + date : date;
-	month = (month<10) ? "0" + month : month;
+	const options = { 
+		weekday: 'long', 
+	    year: 'numeric', 
+	    month: 'long', 
+	    day: 'numeric', 
+	    hour: '2-digit', 
+	    minute: '2-digit', 
+	    second: '2-digit', 
+	    timeZoneName: 'short' 
+	};
 
-	var currentDateTime = date+"/"+month+"/"+year+" "+hours+":"+minutes+":"+seconds;
+	const formattedTime = now.toLocaleDateString('en-US', options);
 
-	document.getElementById("timer").innerHTML = currentDateTime;
+	timerElement.textContent = formattedTime;
 }
-setInterval(updateTime, 1000);
 
+updateTimer();
+
+setInterval(updateTimer, 1000);
 
